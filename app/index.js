ddbc6310b46581262d4ec09bf38b5de6145a6184
antoniehuang <antonieyuchi@gmail.com>
2023-12-28 18:21:35 +0000

    feat: use expo router

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import ImageViewer from './components/ImageViewer';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import ImageViewer from '../components/ImageViewer';
 
-const PlaceholderImage = require('./assets/logos/blobfish.png')
+const PlaceholderImage = require('../assets/logos/blobfish.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <Pressable onPress={() => alert('You pressed a button.')}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} />
+        </Pressable>
       </View>
       <StatusBar style="auto" />
     </View>
