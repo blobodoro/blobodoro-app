27571083806e9c24b1cfa7488c3610f4365893a1
katmilne <katmilne39@gmail.com>
2024-01-15 17:20:07 +0000

    changed nav icons

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,20 +1,36 @@
+import React from "react";
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 const TabsLayout = () => {
-    return (
-        <Tabs>
-            <Tabs.Screen 
-            name="stats" 
-            options={{ title: 'Stats' }} // Customize the title for the "stats" tab
-            />
-            <Tabs.Screen name="fishtank" 
-            options={{ title: 'Fishtank' }} // Customize the title for the "fishtank" tab
-            />
-            <Tabs.Screen name="store" 
-            options={{ title: 'Shop' }} // Customize the title for the "store" tab
-            />
-        </Tabs>
-    )
-}
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="stats"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-stats-chart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="fishtank"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="store"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-basket" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
 
 export default TabsLayout;
